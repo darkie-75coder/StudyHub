6a49130f2738fceeea8f54d9b5c4b67e24489df8
darkie-75coder <shivanshkumargupta74@gmail.com>
2026-07-04 18:30:54 +0530

    fix logout

--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -9,68 +9,81 @@ import { LuLogOut } from "react-icons/lu";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Loader from "./Loader";
 
 const Sidebar = () => {
   const { setLoggedIn } = useContext(AppContext);
   const navigate = useNavigate();
 
+  const [loading, setLoading] = useState(false);
+
   axios.defaults.withCredentials = true;
 
   async function logout() {
     try {
-      const { data } = await axios.get("http://localhost:3000/api/auth/logout");
+      setLoading(true);
+
+      const { data } = await axios.get(
+        "https://studyhub-1ln4.onrender.com/api/auth/logout",
+      );
 
       toast.success(data.message);
       setLoggedIn(false);
       navigate("/");
     } catch (err) {
       toast.error(err.response.data.message);
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <div className="sidebar-box">
-      <ul className="sidebar">
-        <div>
-          <li className="logo-img">
-            <img src={logo} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <ul className="sidebar">
+          <div>
+            <li className="logo-img">
+              <img src={logo} />
+            </li>
+            <li
+              onClick={() => {
+                navigate("/dashboard");
+              }}
+            >
+              <GoHome className="icon" /> Dashboard
+            </li>
+            <li
+              onClick={() => {
+                navigate("/subjects");
+              }}
+            >
+              <LuBookText className="icon" />
+              Subjects
+            </li>
+            <li
+              onClick={() => {
+                navigate("/tasks");
+              }}
+            >
+              <BiTask className="icon" />
+              Tasks
+            </li>
+            <li
+              onClick={() => {
+                navigate("/notes");
+              }}
+            >
+              <LuNotebookPen className="icon" />
+              Notes
+            </li>
+          </div>
+          <li className="last-li" onClick={logout}>
+            <LuLogOut className="icon" /> Logout
           </li>
-          <li
-            onClick={() => {
-              navigate("/dashboard");
-            }}
-          >
-            <GoHome className="icon" /> Dashboard
-          </li>
-          <li
-            onClick={() => {
-              navigate("/subjects");
-            }}
-          >
-            <LuBookText className="icon" />
-            Subjects
-          </li>
-          <li
-            onClick={() => {
-              navigate("/tasks");
-            }}
-          >
-            <BiTask className="icon" />
-            Tasks
-          </li>
-          <li
-            onClick={() => {
-              navigate("/notes");
-            }}
-          >
-            <LuNotebookPen className="icon" />
-            Notes
-          </li>
-        </div>
-        <li className="last-li" onClick={logout}>
-          <LuLogOut className="icon" /> Logout
-        </li>
-      </ul>
+        </ul>
+      )}
     </div>
   );
 };
