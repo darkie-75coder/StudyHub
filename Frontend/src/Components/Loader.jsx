7b8fef763a3314ab1a84e9be1fe752f8dd527848
darkie-75coder <shivanshkumargupta74@gmail.com>
2026-07-05 16:12:16 +0530

    fix bugs

--- Frontend/src/Components/Loader.jsx
+++ Frontend/src/Components/Loader.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div class="dot-spinner">
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
+    <div className="loader">
+      <div class="dot-spinner">
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+        <div class="dot-spinner__dot"></div>
+      </div>
     </div>
   );
 };
